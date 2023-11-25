9da2dc2e51c6fddb82d5ba740f999e40369b3dd4
Sylvain Glaize <mokona@puupuu.org>
2023-11-25 15:23:57 +0100

    Adjust direction keys for AgonLight

--- src/keys.h
+++ src/keys.h
@@ -1,9 +1,22 @@
 #ifndef JEU_LYNX_KEYS_H
 #define JEU_LYNX_KEYS_H
 
+#if defined(__AGON__)
+
+#define KEY_RIGHT 0x15
+#define KEY_DOWN 0x0a
+#define KEY_LEFT 0x08
+#define KEY_UP 0x0b
+
+#endif
+
+#if defined(__LYNX__)
+
 #define KEY_RIGHT 0x09
 #define KEY_DOWN 0x0a
 #define KEY_LEFT 0x08
 #define KEY_UP 0x0b
+
+#endif
 
 #endif//JEU_LYNX_KEYS_H
