c7b81be310774a574b64186c57a06a58ebd55f76
Sylvain Glaize <mokona@puupuu.org>
2023-12-07 23:20:18 +0100

    Display the title text the new way.

--- src/loop_title.c
+++ src/loop_title.c
@@ -1,5 +1,7 @@
 #include "loop_title.h"
 
+#include "../generated/font_data.h"
+#include "../generated/texts_data.h"
 #include "loop_main.h"
 #include "loop_retry.h"
 #include "loop_victory.h"
@@ -14,17 +16,6 @@ extern uint previous_key;
 extern uint key;
 
 #if LANGUAGE == 0
-const char* title_text[] = {
-        "Devant la porte de cette",
-        "prison hant@e vous h@sitez",
-        "encore un instant.",
-        "",
-        "Aurez-vous le courage d'y",
-        "chercher le fameux tr@sor",
-        "dont cet @trange vieillard",
-        "vous a parl@ ?",
-        NULL};
-
 const char* title = "DANS LA PRISON HANTEE";
 const char* author = "Par S.Glaize";
 const char* graphics = "Tuiles par Kenney.nl";
@@ -32,35 +23,12 @@ const char* space_to_enter = "ESPACE pour entrer...";
 
 #elif LANGUAGE == 1
 
-const char * title_text[] = {
-        "In front of the door of this",
-        "haunted prison you hesitate",
-        "for a moment.",
-        "",
-        "Will you have the courage to",
-        "look for the famous treasure",
-        "that this strange old man",
-        "told you about?",
-        NULL};
-
 const char* title = "IN THE HAUNTED PRISON";
 const char* author = "By S.Glaize";
 const char* graphics = "Tiles by Kenney.nl";
 const char* space_to_enter = "SPACE to enter...";
 
 #elif LANGUAGE == 2
-
-const char * title_text[] = {
-        "Antaû la pordo de tiu",
-        "spukita prizono",
-        "vi momenton hezitas.",
-        "",
-        "Ĉu vi havos la kuraĝon",
-        "serĉi la faman trezoron",
-        "kiun tiu stranga maljuna",
-        "viro parolis al vi?",
-        NULL
-};
 
 const char* title = "EN LA SPUKITA PRIZONO";
 const char* author = "De S.Glaize";
@@ -84,11 +52,24 @@ void start_title_loop()
     goto_xy(2, 7);
     print_str(graphics);
 
-    // Display the text
-    for (int i = 0; title_text[i] != NULL; i++)
+    const char * const text = all_texts[TEXT_ENTRY_TEXT];
+    const char * text_pointer = text;
+    char line = 11;
+    char x = 2;
+    while (*text_pointer != '\0')
     {
-        goto_xy(2, 11 + i);
-        print_str(title_text[i]);
+        if (*text_pointer == glyph_count -1)
+        {
+            line++;
+            x = 2;
+        }
+        else
+        {
+            goto_xy(x, line);
+            put_char(*text_pointer);
+            x++;
+        }
+        text_pointer++;
     }
 
     goto_xy(10, 25);
